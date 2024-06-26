1b19fccafb325053decdd37e565b688b3136acbe
robert <robert@arles.us>
2024-06-26 13:31:52 -0700

    astronvim community to custom plugin entries

---
+++ lua/plugins/bash.lua
@@ -0,0 +1,75 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "bash" })
+      end
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(
+        opts.ensure_installed,
+        { "bash-language-server", "shellcheck", "shfmt", "bash-debug-adapter" }
+      )
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "bashls" })
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "shfmt" })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "bash" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        sh = { "shfmt" },
+      },
+      formatters = {
+        shfmt = {
+          prepend_args = function(_, ctx)
+            local args = {}
+            local bo = vim.bo[ctx.buf]
+            if bo.expandtab then
+              local tab_size = bo.tabstop or 2
+              local indent_size = bo.shiftwidth
+              if indent_size == 0 or not indent_size then indent_size = tab_size end
+              vim.list_extend(args, { "-i", tostring(indent_size) })
+            end
+            return args
+          end,
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        sh = { "shellcheck" },
+      },
+    },
+  },
+}
